8234bf7666cbd54d429e9d4f51c1828a7dec6208
abdullahiaa <osuolaleabdullahi@gmail.com>
2022-06-20 18:53:49 +0100

    categories route done

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -14,36 +14,38 @@ def create_app(test_config=None):
     app = Flask(__name__)
     setup_db(app)
 
-    @app.route("/")
-    def index():
-        return jsonify({
-            'message': 'Welcome!',
-        })
-
     """
     @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
-    cors = CORS(app, resources={r"*/api/*": {"origin": "*"}})
+    # cors = CORS(app, resources={r"*/api/*": {"origin": "*"}})
 
     """
     @TODO: Use the after_request decorator to set Access-Control-Allow
     """
-    @app.after_request
-    def after_request(response):
-        response.headers.add(
-            "Access-Control-Allow-Headers", "Content-Type, Authorization"
-        )
-        response.headers.add(
-            "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
-        )
+    # @app.after_request
+    # def after_request(response):
+    #     response.headers.add(
+    #         "Access-Control-Allow-Headers", "Content-Type, Authorization"
+    #     )
+    #     response.headers.add(
+    #         "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+    #     )
 
-        return response
+    #     return response
 
     """
     @TODO:
     Create an endpoint to handle GET requests
     for all available categories.
     """
+    @app.route('/categories')
+    def get_categories():
+        categories = Category.query.all()
+        formatted_categories = [category.format() for category in categories]
+
+        return jsonify({
+            'categories': formatted_categories
+        })
 
     """
     @TODO:
